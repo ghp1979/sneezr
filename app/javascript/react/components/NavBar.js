4a3bb36f3569a5f9aa970880b66dc0b33a12a6c5
ghpelton1979 <ghpelton@gmail.com>
2021-11-22 15:06:00 -0500

    Nav bar links to user auth pages, those link back

--- app/javascript/react/components/NavBar.js
+++ app/javascript/react/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import  {Link } from 'react-router-dom'
 
 const NavBar = props => {
   return(
@@ -8,10 +9,11 @@ const NavBar = props => {
       </div>
       <div class="top-bar rounded">
         <div class="top-bar-left">
-          <h4>Main</h4>
+          <h4><Link to="/">Main</Link></h4>
         </div>
         <div class="top-bar-right">
-          <h5>Sign In/Sign Up</h5>
+          <h5><a href='/users/sign_in'> Sign In </a> / <a href ='/users/sign_up' >Sign Up</a></h5>
+          <a href ='/users/sign_out' data-method='delete'> Sign Out</a>
         </div>
       </div>
     </div>
