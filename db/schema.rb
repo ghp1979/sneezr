4959f7479e212926c74a36f0e02ca5c27ec0448f
ghpelton1979 <ghpelton@gmail.com>
2021-11-22 20:32:14 -0500

    first migration of symptom report

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_191736) do
+ActiveRecord::Schema.define(version: 2021_11_23_011033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "symptom_reports", force: :cascade do |t|
+    t.integer "sneezing", null: false
+    t.integer "congestion", null: false
+    t.integer "runny_nose", null: false
+    t.integer "itchy_nose", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_symptom_reports_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_11_22_191736) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "zip_code", limit: 5, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
