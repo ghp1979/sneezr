3a1027c0a53f9dd85efa8b4c6bb1c4731a48ff29
ghpelton1979 <ghpelton@gmail.com>
2021-11-28 20:43:35 -0500

    cleaned up code a bit

--- app/models/access_api.rb
+++ app/models/access_api.rb
@@ -1,6 +1,4 @@
 require 'faraday'
-require 'json'
-require 'dotenv'
 require './config/environment'
 
 
--- app/models/allergen_report.rb
+++ app/models/allergen_report.rb
@@ -3,7 +3,7 @@ class AllergenReport < ApplicationRecord
   validates :tree_category, presence: true
   validates :grass_value, numericality: true, presence: true
   validates :grass_category, presence: true
-  validates :ragweed_value, numericality: true, presensce: true
+  validates :ragweed_value, numericality: true, presence: true
   validates :ragweed_category, presence: true
   validates :mold_value, numericality: true, presence: true
   validates :mold_category, presence: true
