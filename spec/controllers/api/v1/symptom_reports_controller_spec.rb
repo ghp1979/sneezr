58a68e9f10836789920996153d251260748e5f25
ghpelton1979 <ghpelton@gmail.com>
2021-12-09 15:24:57 -0500

    symptom_reports_controller_spec

--- spec/controllers/api/v1/symptom_reports_controller_spec.rb
+++ spec/controllers/api/v1/symptom_reports_controller_spec.rb
@@ -2,37 +2,34 @@ require "rails_helper"
 
 RSpec.describe Api::V1::SymptomReportsController, type: :controller do
 
-  let!(:test_zip) { ZipCode.create(
-    zip_code: "11111"
-  ) }
-  let!(:test_user) { User.create (
-    email: "Test@testcom",
-    zip_code: "11111",
-    password: "tester"
-  ) }
-  let!(:first_symptom_report) { SymptomReport.create(
-    sneezing: 0,
-    congestion: 0,
-    runny_nose: 0,
-    itchy_nose: 0,
-    user_id: 1,
-    created_at: Date.new(2021, 11, 25),
-  ) }
-  let!(:second_symptom_report) { SymptomReport.create(
-    sneezing: 0,
-    congestion: 1,
-    runny_nose: 3,
-    itchy_nose: 2,
-    user_id: 1,
-    created_at: Date.new(2021, 11, 26),
-  ) }
-
   describe "GETindex" do
-    it "should return date and TNSS for each symptom report"
-
+    it "should return date and TNSS for each symptom report" do
+      zip = FactoryBot.create(:zip_code)
+      user = FactoryBot.create(:user, zip_code_id: zip.id)
+      symptoms1 = FactoryBot.create(:symptom_report, user_id: user.id,
+      created_at: Date.new(2021, 12, 7))
+      symptoms2 = FactoryBot.create(:symptom_report, user_id: user.id,
+      created_at: Date.new(2021, 12, 8))
+      symptoms3 = FactoryBot.create(:symptom_report, user_id: user.id,
+      created_at: Date.new(2021, 12, 9))
+    sign_in user  
     get :index
     returned_json = JSON.parse(response.body)
-    binding.pry
+    date_1 = symptoms1["created_at"]
+    date_2 = symptoms2["created_at"]
+    date_3 = symptoms3["created_at"]
+    tnss1 = (symptoms1["sneezing"] + symptoms1["congestion"] +
+    symptoms1["runny_nose"] + symptoms1["itchy_nose"])
+    tnss2 = (symptoms2["sneezing"] + symptoms2["congestion"] +
+    symptoms2["runny_nose"] + symptoms2["itchy_nose"])
+    tnss3 = (symptoms3["sneezing"] + symptoms3["congestion"] +
+    symptoms3["runny_nose"] + symptoms3["itchy_nose"])
+    expect(returned_json[0][0]).to eq date_1.strftime("%F")
+    expect(returned_json[1][0]).to eq date_2.strftime("%F")
+    expect(returned_json[2][0]).to eq date_3.strftime("%F")
+    expect(returned_json[0][1]).to eq tnss1
+    expect(returned_json[1][1]).to eq tnss2
+    expect(returned_json[2][1]).to eq tnss3
     end
   end
 end
